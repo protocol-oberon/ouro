12b427b98c89b178afffd38f7d4126e6f5234409
Shiloh <ShilohAlleyne@gmail.com>
2026-06-10 14:34:20 +0100

    package ouro with nix
    will be squashed
    
    squash

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "A very basic Haskell dev env flake";
+    description = "Ouro Programming Language";
 
     inputs = {
         nixpkgs-stable.url   = "github:nixos/nixpkgs/nixos-25.11";
@@ -9,22 +9,35 @@
 
     outputs = { self, nixpkgs-stable, nixpkgs-unstable, flake-utils }:
         flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs-stable   = import nixpkgs-stable   { inherit system; };
-            pkgs-unstable = import nixpkgs-unstable { inherit system; };
-        in
-        {
-            devShells.default = pkgs-unstable.mkShell {
-                packages = [
-                    pkgs-unstable.haskellPackages.haskell-language-server
-                    pkgs-unstable.cabal-install
-                    pkgs-unstable.haskell.compiler.ghc910
-                    pkgs-unstable.zlib
-                    pkgs-unstable.haskellPackages.hoogle
-                    pkgs-unstable.stylish-haskell
-                    pkgs-unstable.jq
-                ];
-            };
-        }
-    );
+            let
+                pkgs-stable   = import nixpkgs-stable   { inherit system; };
+                pkgs-unstable = import nixpkgs-unstable { inherit system; };
+
+                # Define the ouro compiler package
+                ouro = pkgs-unstable.haskellPackages.developPackage {
+                    root     = ./.;
+                    modifier = drv: pkgs-unstable.haskell.lib.doJailbreak drv;
+                };
+            in
+            {
+                devShells.default = pkgs-unstable.mkShell {
+                    packages = [
+                        pkgs-unstable.haskellPackages.haskell-language-server
+                        pkgs-unstable.cabal-install
+                        pkgs-unstable.haskell.compiler.ghc910
+                        pkgs-unstable.zlib
+                        pkgs-unstable.haskellPackages.hoogle
+                        pkgs-unstable.stylish-haskell
+                        pkgs-unstable.jq
+                    ];
+                };
+
+                # Export the ouro compiler
+                packages.default = ouro;
+                apps.default     = {
+                    type    = "app";
+                    program = "${ouro}/bin/ouro";
+                };
+            }
+        );
 }
